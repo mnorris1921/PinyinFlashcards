719076d6c1dcec7371ecb07dff18ced8e31f5472
Michael Norris <mnorris@calpoly.edu>
2018-06-09 11:06:42 -0700

    Initial Commit

---
+++ PinyinFlashcards/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PinyinFlashcards
+//
+//  Created by Michael Norris on 6/9/18.
+//  Copyright © 2018 Michael Norris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PinyinFlashcards/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PinyinFlashcards
+//
+//  Created by Michael Norris on 6/9/18.
+//  Copyright © 2018 Michael Norris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
